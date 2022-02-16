5283d109a05c2c403de334cbd90de46f7f5aebd4
mrshiam <mrshiam01@gmail.com>
2022-02-16 16:04:56 +0600

    initial repo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,88 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <navbar
+    :cart="cart"
+    :cart-total="cartTotal"
+    :cart-qty="cartQty"
+    @delete-item="deleteItem"
+  />
+  <div class="container">
+    <router-view
+      :products="products"
+      :cart="cart"
+      @addItem="addItem"
+      @delete-item="deleteItem"
+      :cart-total="cartTotal"
+    />
   </div>
-  <router-view />
 </template>
 
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+  data: function () {
+    return {
+      cart: [],
+      products: [],
+      displayCart: false,
+    };
+  },
+  components: {
+    Navbar,
+  },
+  created() {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+  methods: {
+    addItem(product) {
+      let whichProduct;
+      let existing = this.cart.filter(function (item, index) {
+        if (item.product.id == Number(product.id)) {
+          whichProduct = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+      if (existing.length) {
+        this.cart[whichProduct].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
+    },
+    deleteItem: function (id) {
+      if (this.cart[id].qty > 1) {
+        this.cart[id].qty--;
+      } else {
+        this.cart.splice(id, 1);
+      }
+    },
+  },
+  computed: {
+    cartTotal() {
+      let sum = 0;
+      for (let key in this.cart) {
+        sum = sum + this.cart[key].product.price * this.cart[key].qty;
+      }
+      return sum;
+    },
+    cartQty: function () {
+      let qty = 0;
+      for (let key in this.cart) {
+        qty = qty + this.cart[key].qty;
+      }
+      return qty;
+    },
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+$primary: #6f42c1;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "node_modules/bootstrap/scss/bootstrap";
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <section class="container">
+    <range-selector :products="filteredProducts" v-model="max" />
+
+    <product-list :products="filteredProducts" />
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import RangeSelector from "@/components/RangeSelector";
+import ProductList from "@/components/ProductList";
 export default {
   name: "Home",
+  data: function () {
+    return {
+      max: 50,
+      cart: [],
+    };
+  },
   components: {
-    HelloWorld,
+    RangeSelector,
+    ProductList,
+  },
+  props: ["products"],
+  computed: {
+    filteredProducts() {
+      return this.products.filter((item) => item.price < Number(this.max));
+    },
   },
 };
 </script>
